ff6d2a5e3d3b149c35bbae030a5f779455fdf2d2
Harshini V <harshinikumar2308@gmail.com>
2026-08-04 11:18:56 +0530

    Complete notification module frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import VerifyEmail from "./pages/VerifyEmail";
 import ResendVerification from "./pages/ResendVerification";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
-
+import Notifications from "./pages/Notifications";
 // Dashboard
 import Dashboard from "./pages/Dashboard";
 
@@ -254,6 +254,15 @@ function App() {
               </ProtectedRoute>
             }
         />
+
+        <Route 
+            path="/notifications" 
+            element={ 
+              <ProtectedRoute>
+                <Notifications />
+              </ProtectedRoute>
+            } 
+          />
 
       </Routes>
     </BrowserRouter>
